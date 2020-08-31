2863e483d87ca4bcd3d5070b7ad41f934883a866
caleb <caleb@cashboxng.com>
2020-08-31 13:52:44 +0100

    Updates

--- Program.cs
+++ Program.cs
@@ -1,12 +1,56 @@
 ﻿using System;
+using System.Drawing;
+using System.Text;
 
 namespace Password_Generator
 {
     class Program
     {
+        class PasswordGenerator
+        {
+            private int _size;
+
+            public PasswordGenerator(int size)
+            {
+                this._size = size;
+            }
+
+            public int RandomNumber(int min, int max)
+            {
+                Random random = new Random();
+                var result = random.Next(min, max);
+
+                return result;
+            }
+            public string RandomString ()
+            {
+                StringBuilder builder = new StringBuilder();
+                Random random = new Random();
+
+                char ch;
+                for (int i = 0; i < this._size; i++)
+                {
+                    ch = Convert.ToChar(Convert.ToInt32(Math.Floor(26 * random.NextDouble() + 65)));
+                    builder.Append(ch);
+                }
+                return builder.ToString();
+            }
+
+            public string Password ()
+            {
+                StringBuilder builder = new StringBuilder();
+                builder.Append(this.RandomString());
+                builder.Append(this.RandomNumber(1000, 9999));
+                var result = builder.ToString().ToLower();
+                return result;
+            }
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            PasswordGenerator passwordGenerator = new PasswordGenerator(10);
+
+            Console.WriteLine("Your new password is: {0}", passwordGenerator.Password());
         }
     }
 }
